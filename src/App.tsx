9abdfe1919404eea713f8199440c3576857166a4
git1gt <github1gt@oneco.ru>
2025-10-23 17:09:27 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
         {/* Metrics Grid */}
         <MetricsGrid />
 
-        {/* Chart Section */}
+        {/* Chart Section 
         <div className="mb-12">
           <Card className="bg-white/90 backdrop-blur-sm border-0 shadow-lg">
             <CardHeader className="pb-4">
@@ -138,6 +138,7 @@ function App() {
             </CardContent>
           </Card>
         </div>
+        */}
 
         {/* Team Section */}
         <TeamSection />
