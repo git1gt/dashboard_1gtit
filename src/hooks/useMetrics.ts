d99832cd7c8a6e2a82f23a20b9bfb4d27b25aac9
git1gt <github1gt@oneco.ru>
2025-11-05 13:08:21 +0700

    Updated useMetrics.ts

--- src/hooks/useMetrics.ts
+++ src/hooks/useMetrics.ts
@@ -21,8 +21,8 @@ export function useMetrics() {
 
         // Get current month and year
         const currentDate = new Date();
-        const currentMonthId = 10;
-        //const currentMonthId = currentDate.getMonth() + 1;
+        //const currentMonthId = 10;
+        const currentMonthId = currentDate.getMonth() + 1;
         const currentYear = 2025;
 
         // First, get the year_id for 2025
