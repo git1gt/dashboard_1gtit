e0029365a9f44b501b8187fbc1195fadbbb83c31
git1gt <github1gt@oneco.ru>
2025-11-10 12:33:29 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,8 +42,10 @@ function App() {
               <button 
                 onClick={() => window.location.reload()} 
                 className=" 
-                focus:outline-none
-                focus-visible:ring-2 focus-visible:ring-cyan-500
+                focus:outline-none 
+                active:outline-none 
+                hover:outline-none
+                focus-visible:ring-0я focus-visible:ring-cyan-500
                 transition-transform duration-200 hover:scale-105
                 cursor-pointer bg-transparent">
                 <img 
